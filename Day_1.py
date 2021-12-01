4fe0460c38fbaebf0a412a04500d48991e2dee8f
Filip Adamus <qtsgol@gmail.com>
2021-12-01 13:50:12 +0100

    Day 1 Finish

--- Day_1.py
+++ Day_1.py
@@ -8,6 +8,21 @@ for i in range(1,len(input)):
         
         counter += 1
     
-print(counter)
+print("The answer to Part 1 is: " + str(counter))
 
 #Part 2
+counter = 0
+Shift = 0
+
+for i in range(0,len(input)-2,2):
+    A = int(input[i]) + int(input[i+1]) + int(input[i+2])
+    B = int(input[i+1]) + int(input[i+2]) + int(input[i+3])
+    
+    if A > Shift and Shift is not 0:
+        counter += 1
+    if B > A:
+        counter += 1
+
+    Shift = B    
+
+print("The answer to Part 2 is: " + str(counter))
