42301d2d0fd6c37c747499fbb5bdce75a66b87f3
Filip Adamus <qtsgol@gmail.com>
2021-12-03 13:09:12 +0100

    Day 3 Part 2

--- Day_3.py
+++ Day_3.py
@@ -6,12 +6,10 @@ def main():
     input = open('Inputs/Day_3_Input.txt').read().split() #Get Input
 
     #Part 1
-    Gamma_Rate, Epsilon_Rate = Calculate_Gamma_Epsilon(input)
-    print("Answer to Part 1 is " + str(Gamma_Rate * Epsilon_Rate))
+    print("Answer to Part 1 is " + str(Calculate_Gamma_Epsilon(input)))
 
     #Part 2
-    Calculate_Life_Support(input)
-    #print("Answer to Part 2 is ")
+    print("Answer to Part 2 is " + str(Calculate_Life_Support(input)))
 
     #Finishing touches
     t1 = time.time() #End timer for comaprison
@@ -35,12 +33,51 @@ def Calculate_Gamma_Epsilon(x):
             Gamma_Rate += "0"
             Epsilon_Rate += "1"
     
-    return int(Gamma_Rate, 2), int(Epsilon_Rate, 2)
+    return int(Gamma_Rate, 2) * int(Epsilon_Rate, 2)
 
 def Calculate_Life_Support(x):
 
-    print("Answer to Part 2 is ")
+    oxygen, co2 = x.copy(), x.copy()
 
+    #Oxygen
+    counter = 0
+    j = 0
+    while len(oxygen)>1:
+        placeholder = []
+        for i in oxygen:
+            if int(i[j]) == 1:   
+                counter += 1
+        if counter >= len(oxygen)/2:
+            most_common = "1"
+        else:
+            most_common = "0"
+        for i in oxygen:
+            if i[j] is most_common:
+                placeholder.append(i)
+        oxygen = placeholder
+        counter = 0
+        j += 1
+    
+    #CO2
+    counter = 0
+    j = 0
+    while len(co2)>1:
+        placeholder = []
+        for i in co2:
+            if int(i[j]) == 1:   
+                counter += 1
+        if counter >= len(co2)/2:
+            most_common = "1"
+        else:
+            most_common = "0"
+        for i in co2:
+            if i[j] is not most_common:
+                placeholder.append(i)
+        co2 = placeholder
+        counter = 0
+        j += 1
+
+    return int(oxygen[0], 2) * int(co2[0], 2)
 
 if __name__ == "__main__":
     main()
