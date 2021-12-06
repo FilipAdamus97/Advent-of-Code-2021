76a00539540531cd4472b6707e2b3177fb1c1535
Filip Adamus <qtsgol@gmail.com>
2021-12-06 13:45:15 +0100

    Checkpoint Part 1

--- Day_4.py
+++ Day_4.py
@@ -1,7 +1,23 @@
 import time
 import numpy as np
 
-def part_1(x):
+def part_1(lottery_numbers, boards, markers):
+
+    win = 0
+    for i in lottery_numbers:
+        for x in range(len(boards)):
+            for y in range(len(boards[0])):
+                for z in range(len(boards[0])):
+                    if boards[x][y][z] == i:
+                        markers[x][y][z] = 1
+
+        for x in range(len(boards)):
+            for y in range(len(boards[0])):
+                if np.count_nonzero(markers[x][y]) == 5:
+                    win = 1
+            for y in range(len(boards[0])):
+                if np.count_nonzero(markers[x][:][y]) == 5:
+                    win = 1
 
     return "x"
 
@@ -13,8 +29,8 @@ def sort_input(x):
 
     lottery_numbers = []
     number_of_boards = int((len(x)-1)/25)
-    boards = [np.zeros((5,5)) for i in range(number_of_boards)]
-    markers = boards.copy()
+    boards = [np.zeros((5,5), dtype=int) for i in range(number_of_boards)]
+    markers = [np.zeros((5,5), dtype=int) for i in range(number_of_boards)]
     placeholder = ''
     
 
@@ -28,7 +44,7 @@ def sort_input(x):
     for i in range(number_of_boards):
         for j in range(5):
             for k in range(5):
-                boards[i][j][k] =  int(x[1+25*i+5*j+k]) 
+                boards[i][j][k] = int(np.float64(x[1+25*i+5*j+k]))
  
     return lottery_numbers, boards, markers
 
@@ -36,9 +52,10 @@ def main():
     input = open('Inputs/Day_4_Input.txt').read().split() #Get Input
     t0 = time.time() #Start Timer for comparison
     print("---------------------")
-    lottery_numbers, boards, markers = sort_input(input)
+    
     #Part 1
-    print("Answer to Part 1 is " + str(part_1(input)))
+    lottery_numbers, boards, markers = sort_input(input)
+    print("Answer to Part 1 is " + str(part_1(lottery_numbers, boards, markers)))
 
     #Part 2
     print("Answer to Part 2 is " + str(part_2(input)))
